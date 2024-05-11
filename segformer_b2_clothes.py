957a20dcb2982f75147ea6bcd90b161d74aaea4e
zengjie <514394212@qq.com>
2024-05-11 17:05:08 +0800

    load model lazy init,support models dir

--- segformer_b2_clothes.py
+++ segformer_b2_clothes.py
@@ -1,32 +1,44 @@
 import os
+
 import numpy as np
-from urllib.request import urlopen
-import torchvision.transforms as transforms  
-import folder_paths
-from transformers import SegformerImageProcessor, AutoModelForSemanticSegmentation
-from PIL import Image,ImageOps, ImageFilter
-import torch.nn as nn
 import torch
+import torch.nn as nn
+from PIL import Image
+from transformers import SegformerImageProcessor, AutoModelForSemanticSegmentation
 
-comfy_path = os.path.dirname(folder_paths.__file__)
-custom_nodes_path = os.path.join(comfy_path, "custom_nodes")
+import folder_paths
+
+processor = None
+model = None
 
 
-# 指定本地分割模型文件夹的路径
-model_folder_path = os.path.join(custom_nodes_path,"Comfyui_segformer_b2_clothes","checkpoints","segformer_b2_clothes")
+def load_model():
+    global model
+    global processor
+    if model is not None:
+        return
+    model_folder_paths = folder_paths.get_folder_paths('segformer_b2_clothes')
+    for path in model_folder_paths:
+        if not os.path.exists(path):
+            continue
+        processor = SegformerImageProcessor.from_pretrained(path)
+        model = AutoModelForSemanticSegmentation.from_pretrained(path)
+        return
+    raise Exception("model not found in paths")
 
-processor = SegformerImageProcessor.from_pretrained(model_folder_path)
-model = AutoModelForSemanticSegmentation.from_pretrained(model_folder_path)
 
 def tensor2pil(image):
     return Image.fromarray(np.clip(255. * image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8))
+
 
 # Convert PIL to Tensor
 def pil2tensor(image):
     return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
 
+
 # 切割服装
 def get_segmentation(tensor_image):
+    load_model()
     cloth = tensor2pil(tensor_image)
     # 预处理和预测
     inputs = processor(images=cloth, return_tensors="pt")
@@ -34,35 +46,35 @@ def get_segmentation(tensor_image):
     logits = outputs.logits.cpu()
     upsampled_logits = nn.functional.interpolate(logits, size=cloth.size[::-1], mode="bilinear", align_corners=False)
     pred_seg = upsampled_logits.argmax(dim=1)[0].numpy()
-    return pred_seg,cloth
+    return pred_seg, cloth
 
 
 class segformer_b2_clothes:
-   
+
     def __init__(self):
         pass
-    
+
     # Labels: 0: "Background", 1: "Hat", 2: "Hair", 3: "Sunglasses", 4: "Upper-clothes", 5: "Skirt", 6: "Pants", 7: "Dress", 8: "Belt", 9: "Left-shoe", 10: "Right-shoe", 11: "Face", 12: "Left-leg", 13: "Right-leg", 14: "Left-arm", 15: "Right-arm", 16: "Bag", 17: "Scarf"
-    
+
     @classmethod
     def INPUT_TYPES(cls):
         return {"required":
-                {     
-                 "image":("IMAGE", {"default": "","multiline": False}),
-                 "Face": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
-                 "Hat": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
-                 "Hair": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
-                 "Upper_clothes": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
-                 "Skirt": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
-                 "Pants": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
-                 "Dress": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
-                 "Belt": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
-                 "shoe": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
-                 "leg": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
-                 "arm": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
-                 "Bag": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
-                 "Scarf": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
-                }
+            {
+                "image": ("IMAGE", {"default": "", "multiline": False}),
+                "Face": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
+                "Hat": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
+                "Hair": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
+                "Upper_clothes": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
+                "Skirt": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
+                "Pants": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
+                "Dress": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
+                "Belt": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
+                "shoe": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
+                "leg": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
+                "arm": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
+                "Bag": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
+                "Scarf": ("BOOLEAN", {"default": True, "label_on": "enabled", "label_off": "disabled"}),
+            }
         }
 
     RETURN_TYPES = ("IMAGE",)
@@ -71,13 +83,13 @@ class segformer_b2_clothes:
     FUNCTION = "sample"
     CATEGORY = "CXH"
 
-    def sample(self,image,Face,Hat,Hair,Upper_clothes,Skirt,Pants,Dress,Belt,shoe,leg,arm,Bag,Scarf):
-        
+    def sample(self, image, Face, Hat, Hair, Upper_clothes, Skirt, Pants, Dress, Belt, shoe, leg, arm, Bag, Scarf):
+
         results = []
         for item in image:
-        
+
             # seg切割结果，衣服pil
-            pred_seg,cloth = get_segmentation(item)
+            pred_seg, cloth = get_segmentation(item)
             labels_to_keep = [0]
             # if background :
             #     labels_to_keep.append(0)
@@ -104,15 +116,15 @@ class segformer_b2_clothes:
                 labels_to_keep.append(12)
                 labels_to_keep.append(13)
             if not arm:
-                labels_to_keep.append(14) 
-                labels_to_keep.append(15) 
+                labels_to_keep.append(14)
+                labels_to_keep.append(15)
             if not Bag:
                 labels_to_keep.append(16)
             if not Scarf:
                 labels_to_keep.append(17)
-                
+
             mask = np.isin(pred_seg, labels_to_keep).astype(np.uint8)
-            
+
             # 创建agnostic-mask图像
             mask_image = Image.fromarray(mask * 255)
             mask_image = mask_image.convert("RGB")
